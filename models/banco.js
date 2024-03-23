5e25d7dd3dca136e3dc21089a49e53ef571f89ad
BeatrizAp8 <beatriz.martins108@etec.sp.gov.br>
2024-03-23 18:41:07 -0300

    first commit

---
+++ models/banco.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize("projetoweb", "root", "senha",{
+    host: "localhost",
+    dialect: "mysql"
+})
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
+
